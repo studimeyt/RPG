72efe1381b6f3be397eab24690a265a81d528b12
Harshit Srivastava <harshit.srivastava018@gmail.com>
2022-03-06 21:32:38 +0530

    Character Added

---
+++ RPG/My project/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Variable
+    public float movementSpeed;
+
+
+
+
+
+    //Functions
+    private void Update()
+    {
+        //Player Movement
+        
+    }
+}
